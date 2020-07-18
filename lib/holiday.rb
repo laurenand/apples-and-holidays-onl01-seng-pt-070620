92d014440d29025cad4b7d0e9ebe5b5a7e5ab7ed
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 19:33:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     supplies_hash.each do |holiday, data|
       if holiday == :memorial_day 
         data << "Grill"
-        data << "Table Cloth"
+        data
       end
     end
   end
