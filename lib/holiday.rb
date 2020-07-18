589db99278632add74a16ee27b2e3fd5224b6f8a
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 23:21:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! << ":"
     holidays.each do |holiday, supplies|
       puts "  #{holiday.to_s.split("_").map {|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
-     
+     binding.pry
     end
   end
 end
