671067ba47ff31d6e901f2b36cbb8899733b229b
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 19:59:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall]<< :columbus_day
+  holiday_hash[:fall]<< holiday_name
   # remember to return the updated hash
   
 end
