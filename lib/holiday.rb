1131350dc5c7ba489af52f1a26116a5c45e04aa2
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 19:04:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,8 +17,8 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash.each do |supply|
-    if 
+  holiday_hash.each do |holiday|
+    if holiday == 
   
 end
 
