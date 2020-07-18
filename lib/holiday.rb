7c8d8f9903f43cb3f3cf0165677ff10abb721ff2
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 23:28:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   bbq = []
-  holiday_hash.each do |season, |
+  holiday_hash.each do |season, var1|
+    binding.pry
+  end
 end
 
 
