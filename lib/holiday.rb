cac94e92360c4d6c9215ce04f8f7b07837c2225a
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 23:09:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,8 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! << ":"
       puts holiday.keys[0].to_s.capitalize! << ":"
       binding.pry
-      end
-    end
+      
   end
 end
 
