8a5119c7e6ea48f998f3ea1df5661885891202d4
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 20:55:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     if season == :winter
-      season.to_s.
+      season.to_s.capitalize!
   binding.pry
   end
 end
