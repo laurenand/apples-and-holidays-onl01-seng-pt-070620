7b4151fa9d0375d18e793611d382391b90e2fb8a
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 20:05:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,9 +29,7 @@ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash.each do |season|
-    if season == :fall
-      
+  holiday_hash[season]
       binding.pry 
   
   # remember to return the updated hash
