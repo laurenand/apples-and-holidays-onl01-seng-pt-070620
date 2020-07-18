0f9a3f83c23645bd9624a424ca0f8ec2590bffe2
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-18 19:11:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     
     binding.pry 
     end
-  end
+  
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
